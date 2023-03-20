8f613abbbae96d4007848af52e5d6091948d354c
Guilherme Aquino <guiiaquino92@gmail.com>
2023-03-19 22:45:14 -0300

    feat: pagination logic

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,51 +1,77 @@
-import { createServer, Factory, Model } from 'miragejs'
-import { faker } from '@faker-js/faker';
-
-type User = {
+import {
+    createServer,
+    Factory,
+    Model,
+    Response,
+    ActiveModelSerializer,
+  } from 'miragejs';
+import { faker } from '@faker-js/faker';  
+  
+  type User = {
     name: string;
     email: string;
     created_at: string;
-}
-
-/**
- * It creates a server that has a `/api/users` route that returns a list of users
- * and a `/users` route that returns a list of users
- * @returns A server instance
- */
-
-export function makeServer() {
+  };
+  
+  function makeServer() {
     const server = createServer({
-        models: {
-            user: Model.extend<Partial<User>>({})
-        },
-
-        factories: {
-            user: Factory.extend({
-                name(i) { return faker.internet.userName() },
-                email() { 
-                    return faker.internet.email().toLowerCase(); 
-                },
-                createdAt() { 
-                    return faker.date.recent(10);
-                },
-            })
-        },
-
-        seeds(server) {
-            server.createList('user',10)
-        },
-
-        routes() {
-            this.namespace = 'api'
-            this.timing = 750;
-
-            this.get('/users');
-            this.post('/users');
-
-            this.namespace = '';
-            this.passthrough();
-        }
-    })
-
+      serializers: {
+        application: ActiveModelSerializer,
+      },
+  
+      models: {
+        user: Model.extend<Partial<User>>({}),
+      },
+  
+      factories: {
+        user: Factory.extend({
+          name() {
+            return faker.name.findName();
+          },
+          email() {
+            return faker.internet.email().toLocaleLowerCase();
+          },
+          created_at() {
+            return faker.date.recent(10);
+          },
+        }),
+      },
+  
+      seeds(server) {
+        server.createList('user', 20);
+      },
+  
+      routes() {
+        this.namespace = 'api';
+        this.timing = 750;
+  
+        this.get('/users', function (schema, request) {
+          const { page = 1, per_page = 10 } = request.queryParams;
+  
+          const pageAsNumber = Number(page);
+          const perPageAsNumber = Number(per_page);
+  
+          const total = schema.all('user').length;
+  
+          const pageStart = (pageAsNumber - 1) * perPageAsNumber;
+          const pageEnd = pageStart + perPageAsNumber;
+  
+          const users = this.serialize(schema.all('user')).users.slice(
+            pageStart,
+            pageEnd,
+          );
+  
+          return new Response(200, { 'x-total-count': String(total) }, { users });
+        });
+        this.get('/users/:id');
+        this.post('/users');
+  
+        this.namespace = '';
+        this.passthrough();
+      },
+    });
+  
     return server;
-}
+  }
+  
+  export { makeServer };
